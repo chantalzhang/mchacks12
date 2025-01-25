f5de20462ca13c32c93d8f5ea2d78a3ea4d10adf
kathulul <kathyliu5321@gmail.com>
2025-01-25 17:12:46 -0500

    landing page is done

--- static/scripts/landing.js
+++ static/scripts/landing.js
@@ -1,6 +1,5 @@
 const startGameButton = document.querySelector('.start-game');
 const howToPlayButton = document.querySelector('.how-to-play');
-const saveFilesButton = document.querySelector('.save-files');
 
 startGameButton.addEventListener('click', () => {
   // Add logic to start the game
@@ -10,9 +9,4 @@ startGameButton.addEventListener('click', () => {
 howToPlayButton.addEventListener('click', () => {
   // Redirect to the "How to Play" page
   window.location.href = 'how-to-play.html';
-});
-
-saveFilesButton.addEventListener('click', () => {
-  // Add logic to handle save files
-  console.log('Accessing save files...');
 });
