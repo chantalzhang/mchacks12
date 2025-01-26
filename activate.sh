7d3980492a685b1d9b8de51f4731771b8180cbc3
kathulul <kathyliu5321@gmail.com>
2025-01-25 22:47:33 -0500

    100 words

--- activate.sh
+++ activate.sh
@@ -3,7 +3,7 @@
 # Check if venv exists, if not create it
 if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
-    python -m venv venv
+    python3 -m venv venv
 fi
 
 # Install/update requirements
