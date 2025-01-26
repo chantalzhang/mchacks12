99cf571877ed6a138d7992ed954d5c11e7398f5a
kathulul <kathyliu5321@gmail.com>
2025-01-26 00:51:07 -0500

    mappings

---
+++ mappings.py
@@ -0,0 +1,58 @@
+class SpaceLocationMap:
+    MAP = {
+        "spaceship_control_room": "/static/images/locations/space/homeship.webp",
+        "spaceship_lookout": "/static/images/locations/space/shiplookout.webp",
+        "moon": "/static/images/locations/space/moon.webp",
+        "outer space": "/static/images/locations/space/space.webp",
+        "frozen_planet": "/static/images/locations/space/frozenplanet.webp",
+        "desert_planet": "/static/images/locations/space/desert.webp",
+        "lush_planet": "/static/images/locations/space/alientropic.webp",
+        "dark_planet": "/static/images/locations/space/darkplanet.webp"
+    }
+class FantasyLocationMap:
+    MAP = {
+        "castle_bedroom": "/static/images/locations/fantasy/castle_bedroom.webp",
+        "castle_dungeon": "/static/images/locations/fantasy/castle_dungeon.webp",
+        "castle_lobby": "/static/images/locations/fantasy/castle_lobby.webp",
+        "castle_library": "/static/images/locations/fantasy/castle_library.webp",
+        "cave": "/static/images/locations/fantasy/cave.webp",
+        "forest_swamp": "/static/images/locations/fantasy/forest_swamp.webp",
+        "forest": "/static/images/locations/fantasy/forest.webp",
+        "sky": "/static/images/locations/fantasy/sky.webp",
+        "forest_castle": "/static/images/locations/fantasy/forest.webp",
+    }
+
+class SpaceNPCMap:
+    MAP = {
+        "mysterious_woman": "/static/images/npcs/space/change.png",
+        "beautiful_woman": "/static/images/npcs/space/change.png",
+        "moon_goddess": "/static/images/npcs/space/change.png",
+        "demon": "/static/images/npcs/space/demon.png",
+        "monster": "/static/images/npcs/space/demon.png",
+        "hostile_alien": "/static/images/npcs/space/demon.png",
+        "friendly_space_cat": "/static/images/npcs/space/spacecat.png",
+        "space_merchant": "/static/images/npcs/space/spacecat.png",
+        "researcher": "/static/images/npcs/space/researcher.png",
+        "lush_planet_inhabitant": "/static/images/npcs/space/researcher.png",
+        "dark_planet_inhabitant": "/static/images/npcs/space/demon.png",
+        "frozen_planet_inhabitant": "/static/images/npcs/space/spacesuit.png",
+        "moon_inhabitant": "/static/images/npcs/space/change.png",
+        "engineer": "/static/images/npcs/space/researcher.png",
+        "astronaut": "/static/images/npcs/space/spacesuit.png",
+        "scientist": "/static/images/npcs/space/researcher.png",
+        "lost_astronaut": "/static/images/npcs/space/spacesuit.png",
+
+    }
+class FantasyNPCMap:
+    MAP = {
+        "mysterious_figure": "/static/images/npcs/fantasy/cloaked.png",
+        "hostile_entity": "/static/images/npcs/fantasy/cloaked.png",
+        "chaotic_entity": "/static/images/npcs/fantasy/mushroom.png",
+        "talking_mushroom": "/static/images/npcs/fantasy/mushroom.png",
+        "wise_old_man": "/static/images/npcs/fantasy/wiseman.png",
+        "professor": "/static/images/npcs/fantasy/wiseman.png",
+        "friendly_witch": "/static/images/npcs/fantasy/witch.png",
+        "student": "/static/images/npcs/fantasy/witch.png",
+        "king": "/static/images/npcs/fantasy/fatking.png",
+
+    }
